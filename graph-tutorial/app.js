3f20c83982b45ff6e3a5fce3a90ead58cc95f555
Tuyen Dang <tuyendq@gmail.com>
2020-03-09 20:56:50 +0700

    step 3/5: Add Azure AD authentication - Implement Sign In

--- graph-tutorial/app.js
+++ graph-tutorial/app.js
@@ -6,9 +6,73 @@ var logger = require('morgan');
 var session = require('express-session');
 var flash = require('connect-flash');
 require('dotenv').config();
+var graph = require('./graph');
+
+var passport = require('passport');
+var OIDCStrategy = require('passport-azure-ad').OIDCStrategy;
+
+// Configure passport
+
+// In-memory storage of logged-in users
+// For demo purposes onlye, production apps should store
+// this in a reliable storage
+var users = {};
+
+// Passport calls serializeUser and deserializeUser to
+// manage users
+passport.serializeUser(function(user, done) {
+  // Use the OID property of the user as a key
+  users[user.profile.oid] = user;
+  done (null, user.profile.oid);
+});
+
+passport.deserializeUser(function(id, done) {
+  done(null, users[id]);
+});
+
+// Callback function called once the sign-in is complete
+// and an access token has been obtained
+async function signInComplete(iss, sub, profile, accessToken, refreshToken, params, done) {
+  if (!profile.oid) {
+    return done(new Error("No OID found in user profile."), null);
+  }
+
+  try {
+    const user = await graph.getUserDetails(accessToken);
+
+    if (user) {
+      // Add properties to profile
+      profile['email'] = user.mail ? user.mail : user.userPrincipalName;
+    }
+  } catch(err) {
+    done(err, null);
+  }
+
+  // Save the profile and tokens in user storage
+  users[profile.oid] = { profile, accessToken };
+  return done(null, users[profile.oid]);
+}
+
+// Configure OIDC strategy
+passport.use(new OIDCStrategy(
+  {
+    identityMetadata: `${process.env.OAUTH_AUTHORITY}${process.env.OAUTH_ID_METADATA}`,
+    clientID: process.env.OAUTH_APP_ID,
+    responseType: 'code id_token',
+    responseMode: 'form_post',
+    redirectUrl: process.env.OAUTH_REDIRECT_URI,
+    allowHttpForRedirectUrl: true,
+    clientSecret: process.env.OAUTH_APP_PASSWORD,
+    validateIssuer: false,
+    passReqToCallback: false,
+    scope: process.env.OAUTH_SCOPES.split(' ')
+  },
+  signInComplete
+));
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth.js');
 
 var app = express();
 
@@ -51,7 +115,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function(req, res, next) {
+  // Set the authenticated user in the
+  // template locals
+  if (req.user) {
+    res.locals.user = req.user.profile;
+  }
+  next();
+});
+
 app.use('/', indexRouter);
+app.use('/auth', authRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
